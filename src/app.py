b75edb21546c698e4357f7ea8eb4190bd6aa1959
Fergodie <Fero.die@gmail.com>
2024-02-14 20:09:53 +0000

    apis

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet # se importa toodo lo  que se cree en models
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,69 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+#--------------------------------------------------------character---------------------------------------------
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/character', methods=['GET'])
+def get_character():
+    all_character = Character.query.all()
+    results = []
+    for character in all_character:
+        results.append(character.serialize())
 
-    return jsonify(response_body), 200
+   
+
+    return jsonify(results), 200
+
+@app.route('/character/<int:character_id>', methods=['GET']) # busca un solo autor
+def get_characterId(character_id):
+    character = Character.query.get(character_id)
+    if character is None:
+        return jsonify(), 404
+    
+    return jsonify(character.serialize()), 200
+
+#---------------------------------------------planet--------------------------------------------------
+
+@app.route('/planet', methods=['GET'])
+def get_planet():
+    all_planet = Planet.query.all()
+    results = []
+    for planet in all_planet:
+        results.append(planet.serialize())
+
+   
+
+    return jsonify(results), 200
+
+@app.route('/planet/<int:planet_id>', methods=['GET']) # busca un solo autor
+def get_planetId(character_id):
+    planet = Planet.query.get(character_id)
+    if planet is None:
+        return jsonify(), 404
+    
+    return jsonify(planet.serialize()), 200
+  
+
+"""@app.route('/autores/<int:autor_id>/libros', methods=['POST']) # crear un libro
+def create_libro(autor_id):
+    body = request.get_json()
+
+    libro = Libro()
+    libro.ISBN = body['ISBN'] # body.get('ISBN')
+    #se agrega todo lo que este en models/libtos los atributos
+    if body.get('titulo') is None:
+        libro.titulo = "Sin titulo"
+    else:
+        libro.titulo = body.get = "Sin titulo" # poder dejar poner algo sin tener que ser obligatorio
+
+    db.session.add(libro)
+    db.session.commit()
+
+    return jsonify("libro creado"), 200"""
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -17,3 +17,59 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False )
+    genero = db.Column(db.Enum('masculino','femenino', name='Genero'), nullable=False)
+    hair_color = db.Column(db.Enum('negro','rubio', name='HairColor'), nullable=False)
+    eye_color = db.Column(db.Enum('verdes','azules', name='EyeColor'), nullable=False)
+
+    def __repr__(self):
+        return '<Character %r>' % self.nombre
+
+    def serialize(self): #aca va todos lo que este en class libro
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "genero": self.genero,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color
+            # do not serialize the password, its a security breach
+        }
+    
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # nombre agregar
+    population = db.Column(db.Integer, nullable=False)
+    terrain = db.Column(db.Integer, nullable=False)
+    
+
+    def __repr__(self):
+        return '<Planet %r>' % self.id
+
+    def serialize(self): #aca va todos lo que este en class libro
+        return {
+            "id": self.id,
+            "population": self.population,
+            "terrain": self.terrain
+            
+            # do not serialize the password, its a security breach
+        }
+    
+class Favorito(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planetId = db.Column(db.Integer, db.ForeignKey("planet.id"), nullable=True)
+    characterId = db.Column(db.Integer, db.ForeignKey("character.id"), nullable=True)
+    
+
+    def __repr__(self):
+        return '<Favorite %r>' % self.id
+
+    def serialize(self): #aca va todos lo que este en class libro
+        return {
+            "id": self.id,
+            "population": self.population,
+            "terrain": self.terrain
+            
+            # do not serialize the password, its a security breach
+        }
